147aee824279457ae828aeeb366c4090c22308d0
FirescuOvidiu <ovidiufirescu@gmail.com>
2019-11-10 11:50:40 +0200

    Starter project structure

---
+++ CustomAllocator/CustomAllocator/CustomAllocator.cpp
@@ -0,0 +1,28 @@
+
+#include "stdafx.h"
+#include "CustomAllocator.h"
+
+//----------------------------------------------------------------------------
+
+void * __cdecl CustomAllocator_New(size_t aSize, int aBlockUse, char const * aFileName, int aLineNumber)
+{
+  return CustomAllocator_Malloc(aSize, aBlockUse, aFileName, aLineNumber);
+}
+
+void __cdecl CustomAllocator_Delete(void * aBlock, int aBlockUse, char const * aFileName, int aLineNumber) noexcept
+{
+  CustomAllocator_Free(aBlock, aBlockUse, aFileName, aLineNumber);
+}
+
+void * __cdecl CustomAllocator_Malloc(size_t aSize, int aBlockUse, char const * aFileName, int aLineNumber)
+{
+  // default CRT implementation
+  return _malloc_dbg(aSize, aBlockUse, aFileName, aLineNumber);
+}
+
+void __cdecl CustomAllocator_Free(void * aBlock, int aBlockUse, char const * /*aFileName*/, int /*aLineNumber*/)
+{
+  // default CRT implementation
+  _free_dbg(aBlock, aBlockUse);
+}
+
