19ec1b2a2d482b02e9c70bc37f33dbc4428f0bd6
FirescuOvidiu <ovidiufirescu@gmail.com>
2019-11-15 23:34:12 +0200

    Added data members, constructor and destructor for the class Logger

--- CustomAllocator/CustomAllocator/Logger.cpp
+++ CustomAllocator/CustomAllocator/Logger.cpp
@@ -1,2 +1,15 @@
 #include "stdafx.h"
-#include "Logger.h"
+
+
+Logger::Logger()
+{
+	m_loggerFile.open("LogFile.log", std::ios::out | std::ios::app);
+	m_logLevel = LogLevel::Log_Level_Info;
+	m_logType = LogType::File_Log;
+}
+
+
+Logger::~Logger()
+{
+	m_loggerFile.close();
+}
--- CustomAllocator/CustomAllocator/Logger.h
+++ CustomAllocator/CustomAllocator/Logger.h
@@ -1,6 +1,33 @@
 #pragma once
 
+#include "stdafx.h"
+
+typedef enum class Log_Levels
+{
+	Disable_Log = 1,
+	Log_Level_Info = 2,
+	Log_Level_Warning = 3,
+	Log_Level_Error = 4,
+	Log_Level_Debug = 5
+}LogLevel;
+
+
+typedef enum class Log_Types
+{
+	No_Log = 1,
+	Console_Log = 2,
+	File_Log = 3
+}LogType;
+
+
 class Logger
 {
-};
+public:
+	Logger();
+	~Logger();
 
+private:
+	std::ofstream m_loggerFile;
+	LogLevel m_logLevel;
+	LogType m_logType;
+};
