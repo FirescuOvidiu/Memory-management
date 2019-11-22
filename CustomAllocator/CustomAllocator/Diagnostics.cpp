9d2264abc6752e941ffadb72ab1a92c24c5109ee
FirescuOvidiu <ovidiufirescu@gmail.com>
2019-11-22 13:23:09 +0200

    Created constructor and destructor for class Diagnostics

--- CustomAllocator/CustomAllocator/Diagnostics.cpp
+++ CustomAllocator/CustomAllocator/Diagnostics.cpp
@@ -1,13 +1,13 @@
 #include "stdafx.h"
 
 
-Diagnostics::Diagnostics()
+Diagnostics::Diagnostics() : totalMemory(0), maxMemoryUtilization(0), avgMemoryUtilization(0), avgMaxContinousMemory(0)
 {
-
+	diagFile.open("DiagnosticsFile.diag", std::ofstream::out);
 }
 
 
 Diagnostics::~Diagnostics()
 {
-
+	diagFile.close();
 }
--- CustomAllocator/CustomAllocator/Diagnostics.h
+++ CustomAllocator/CustomAllocator/Diagnostics.h
@@ -2,14 +2,21 @@
 #include "stdafx.h"
 
 
+/*
+	This class is used to evaluate the fragmentation state of the memory pool
+	It also provides informations about the memory utilization
+*/
 class Diagnostics
 {
 	Diagnostics();
 	~Diagnostics();
+
 private:
-	std::ofstream m_diagFile;
+	std::ofstream diagFile;
 	int totalMemory;
 	int maxMemoryUtilization;
 	double avgMemoryUtilization;
+	double avgMaxContinousMemory;
 
+	// need to invent a metric to evaluate the fragmentation state of the memory pool
 };
