f8107de2391d4d3399b27e56508c8334002f3421
FirescuOvidiu <ovidiufirescu@gmail.com>
2019-12-26 19:40:09 +0200

    Add members to the class DiagnoseExternalFragmentation

--- CustomAllocator/CustomAllocator/DiagnoseExternalFragmentation.h
+++ CustomAllocator/CustomAllocator/DiagnoseExternalFragmentation.h
@@ -2,5 +2,12 @@
 
 class DiagnoseExternalFragmentation
 {
+public:
+	DiagnoseExternalFragmentation() : totalMemory(0), nrFragmentation(0), avgFragmentation(0) {}
 
+private:
+	std::ofstream diagFile;
+	int totalMemory;
+	int nrFragmentation;
+	double avgFragmentation;
 };
