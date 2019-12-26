af6552874ed0a432188d66c627bb5415ef27a38a
FirescuOvidiu <ovidiufirescu@gmail.com>
2019-12-26 19:48:38 +0200

    Add methods to the class DiagnoseExternalFragmentation

--- CustomAllocator/CustomAllocator/DiagnoseExternalFragmentation.cpp
+++ CustomAllocator/CustomAllocator/DiagnoseExternalFragmentation.cpp
@@ -1,1 +1,17 @@
 #include "stdafx.h"
+
+
+void DiagnoseExternalFragmentation::initExternalFrag(const int _totalMemory)
+{
+	totalMemory = _totalMemory;
+}
+
+
+void DiagnoseExternalFragmentation::updateExternalFrag(const int totalMemoryAvailable, const int biggestContMemory)
+{
+	double currFragmentation{ 0 };
+
+	currFragmentation = 1 - (double)biggestContMemory / totalMemoryAvailable;
+	avgFragmentation += currFragmentation;
+	nrFragmentation++;
+}
--- CustomAllocator/CustomAllocator/DiagnoseExternalFragmentation.h
+++ CustomAllocator/CustomAllocator/DiagnoseExternalFragmentation.h
@@ -1,9 +1,13 @@
 #pragma once
 
+
 class DiagnoseExternalFragmentation
 {
 public:
 	DiagnoseExternalFragmentation() : totalMemory(0), nrFragmentation(0), avgFragmentation(0) {}
+
+	void initExternalFrag(const int _totalMemory);
+	void updateExternalFrag(const int totalMemoryAvailable, const int biggestContMemory);
 
 private:
 	std::ofstream diagFile;
