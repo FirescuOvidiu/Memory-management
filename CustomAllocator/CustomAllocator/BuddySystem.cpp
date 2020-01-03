84a29d7311beddabd2d3785fb2f33ee2ab36d496
FirescuOvidiu <ovidiufirescu@gmail.com>
2020-01-03 13:54:27 +0200

    Add initial methods that need to be implemented in the BuddySystem class

--- CustomAllocator/CustomAllocator/BuddySystem.cpp
+++ CustomAllocator/CustomAllocator/BuddySystem.cpp
@@ -1,2 +1,25 @@
 #include "stdafx.h"
 
+
+BuddySystem::BuddySystem(size_t poolSize)
+{
+
+}
+
+
+void* __cdecl BuddySystem::allocMemory(size_t aSize, int aBlockUse, char const*, int)
+{
+	return nullptr;
+}
+
+
+void __cdecl BuddySystem::freeMemory(void* aBlock, int)
+{
+
+}
+
+
+BuddySystem::~BuddySystem()
+{
+
+}
--- CustomAllocator/CustomAllocator/BuddySystem.h
+++ CustomAllocator/CustomAllocator/BuddySystem.h
@@ -3,5 +3,11 @@
 
 class BuddySystem : public Strategy, public DiagnosticTools
 {
+public:
+	BuddySystem(size_t poolSize);
 
+	void* __cdecl allocMemory(size_t aSize, int aBlockUse, char const* /*aFileName*/, int /*aLineNumber*/) override;
+	void __cdecl freeMemory(void* aBlock, int /*aBlockUse*/) override;
+
+	~BuddySystem();
 };
