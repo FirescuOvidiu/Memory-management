6d0b1a7b0c9f921a50b4cd900f1a0899f527293e
FirescuOvidiu <ovidiufirescu@gmail.com>
2020-03-21 18:17:21 +0200

    Added comments, modified variables

--- CustomAllocator/CustomAllocatorTest/GenerateTestUnits.cpp
+++ CustomAllocator/CustomAllocatorTest/GenerateTestUnits.cpp
@@ -1,16 +1,26 @@
 #include "stdafx.h"
 
-
+/*
+	Method used to generate test units based on a distribution
+	How we generate the test units:
+		- allocate X objects at the start (X is decided by us)
+		- while we haven't made a total of Z allocations: (Z is decided by us)
+			- deallocate Y random objects (Y is random)
+			- allocate   Y random objects
+		- deallocate all objects
+*/
 void GenerateTestUnits::generateTU()
 {
 	outputTU.open("generatedTU.txt", std::ofstream::out);
 
+	// Useful informations about the TU generated
 	outputTU << "\n\nPool size: " << poolSize << "\n";
 	outputTU << "Number of allocations: " << numberAllocations << "\n";
 	outputTU << "The size of objects allocated are between: " << rangeObjectSize.first << " and " << rangeObjectSize.second << "\n";
 	outputTU << "Allocation format: Allocate objectId objectSize \n";
 	outputTU << "Deallocation format: Deallocate objectId \n\n";
 
+	// Select the distribution
 	std::random_device rd;
 	std::uniform_int_distribution<int> distribution(rangeObjectSize.first, rangeObjectSize.second);
 	// Need to add more distributions
@@ -18,25 +28,30 @@ void GenerateTestUnits::generateTU()
 	std::set<int> storeObjectId;
 	int countAllocations = 0;
 
-	for (int it = 0; it < numberObjectsAllocated && countAllocations < numberAllocations; it++)
+	// Allocate X objects
+	for (int objectId = 0; objectId < numberObjectsAllocated && countAllocations < numberAllocations; objectId++)
 	{
-		outputTU << "Allocate " << it << " " << distribution(rd) << "\n";
+		outputTU << "Allocate " << objectId << " " << distribution(rd) << "\n";
 		countAllocations++;
 	}
 
+	// wWile we haven't made a total of Z allocations
 	while (countAllocations < numberAllocations)
 	{
+		// Deallocate Y random objects
 		deallocateObjects(countAllocations, storeObjectId);
 
-		for (const auto& itVisited : storeObjectId)
+		// Allocate Y random objects
+		for (const auto& objectId : storeObjectId)
 		{
-			outputTU << "Allocate " << itVisited << " " << distribution(rd) << "\n";
+			outputTU << "Allocate " << objectId << " " << distribution(rd) << "\n";
 			countAllocations++;
 		}
 
 		storeObjectId.clear();
 	}
 
+	// Deallocate all objects
 	for (int it = 0; it < numberObjectsAllocated; it++)
 	{
 		outputTU << "Deallocate " << it << "\n";
@@ -52,14 +67,19 @@ void GenerateTestUnits::loadTU()
 }
 
 
+/*
+	Method used to deallocate Y random objects (Y is decided by us)
+*/
 void GenerateTestUnits::deallocateObjects(const int countAllocations, std::set<int>& storeObjectId)
 {
 	std::random_device rd;
 	std::uniform_int_distribution<int> getNumberDealloc(rangeNumberDeallocations.first, rangeNumberDeallocations.second);
 	std::uniform_int_distribution<int> getObjectId(0, numberObjectsAllocated - 1);
+	// generate the number of deallocations (Y)
 	int numberDeallocations = std::min(getNumberDealloc(rd), numberAllocations - countAllocations);
 	int objectId = 0;
 
+	// Deallocate Y objects
 	for (int it = 0; it < numberDeallocations; it++)
 	{
 		do
