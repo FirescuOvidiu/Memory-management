cc662b7798297868b9abd5bffa410b9d1093c8ad
FirescuOvidiu <ovidiufirescu@gmail.com>
2020-03-21 18:04:35 +0200

    Implemented method generateTU in GenerateTestUnits

--- CustomAllocator/CustomAllocatorTest/GenerateTestUnits.cpp
+++ CustomAllocator/CustomAllocatorTest/GenerateTestUnits.cpp
@@ -3,11 +3,71 @@
 
 void GenerateTestUnits::generateTU()
 {
+	outputTU.open("generatedTU.txt", std::ofstream::out);
 
+	outputTU << "\n\nPool size: " << poolSize << "\n";
+	outputTU << "Number of allocations: " << numberAllocations << "\n";
+	outputTU << "The size of objects allocated are between: " << rangeObjectSize.first << " and " << rangeObjectSize.second << "\n";
+	outputTU << "Allocation format: Allocate objectId objectSize \n";
+	outputTU << "Deallocation format: Deallocate objectId \n\n";
+
+	std::random_device rd;
+	std::uniform_int_distribution<int> distribution(rangeObjectSize.first, rangeObjectSize.second);
+	// Need to add more distributions
+
+	std::set<int> storeObjectId;
+	int countAllocations = 0;
+
+	for (int it = 0; it < numberObjectsAllocated && countAllocations < numberAllocations; it++)
+	{
+		outputTU << "Allocate " << it << " " << distribution(rd) << "\n";
+		countAllocations++;
+	}
+
+	while (countAllocations < numberAllocations)
+	{
+		deallocateObjects(countAllocations, storeObjectId);
+
+		for (const auto& itVisited : storeObjectId)
+		{
+			outputTU << "Allocate " << itVisited << " " << distribution(rd) << "\n";
+			countAllocations++;
+		}
+
+		storeObjectId.clear();
+	}
+
+	for (int it = 0; it < numberObjectsAllocated; it++)
+	{
+		outputTU << "Deallocate " << it << "\n";
+	}
+
+	outputTU.close();
 }
 
 
 void GenerateTestUnits::loadTU()
 {
 
+}
+
+
+void GenerateTestUnits::deallocateObjects(const int countAllocations, std::set<int>& storeObjectId)
+{
+	std::random_device rd;
+	std::uniform_int_distribution<int> getNumberDealloc(rangeNumberDeallocations.first, rangeNumberDeallocations.second);
+	std::uniform_int_distribution<int> getObjectId(0, numberObjectsAllocated - 1);
+	int numberDeallocations = std::min(getNumberDealloc(rd), numberAllocations - countAllocations);
+	int objectId = 0;
+
+	for (int it = 0; it < numberDeallocations; it++)
+	{
+		do
+		{
+			objectId = getObjectId(rd);
+		} while ((storeObjectId.find(objectId) != storeObjectId.end()));
+
+		outputTU << "Deallocate " << objectId << "\n";
+		storeObjectId.insert(objectId);
+	}
 }
--- CustomAllocator/CustomAllocatorTest/GenerateTestUnits.h
+++ CustomAllocator/CustomAllocatorTest/GenerateTestUnits.h
@@ -4,17 +4,22 @@
 class GenerateTestUnits
 {
 public:
-	GenerateTestUnits(int poolSize, int numberObjectsAllocated, std::pair<int, int> ObjectSize, std::pair<int, int> numberDeallocations) :
-		poolSize(poolSize), numberObjectsAllocated(numberObjectsAllocated), ObjectSize(ObjectSize), numberDeallocations(numberDeallocations) {}
+	GenerateTestUnits(int poolSize, int numberAllocations, int numberObjectsAllocated, std::pair<int, int> ObjectSize, std::pair<int, int> rangeNumberDeallocations) :
+		poolSize(poolSize), numberAllocations(numberAllocations), numberObjectsAllocated(numberObjectsAllocated), rangeObjectSize(ObjectSize), rangeNumberDeallocations(rangeNumberDeallocations) {}
 
 	void generateTU();
 	void loadTU();
 
 private:
+
+	void deallocateObjects(const int countAllocations, std::set<int>& storeObjectId);
+
+private:
 	std::ofstream outputTU;
 	std::ifstream inputTU;
 	int poolSize;
+	int numberAllocations;
 	int numberObjectsAllocated;
-	std::pair<int, int> ObjectSize;
-	std::pair<int, int> numberDeallocations;
+	std::pair<int, int> rangeObjectSize;
+	std::pair<int, int> rangeNumberDeallocations;
 };
