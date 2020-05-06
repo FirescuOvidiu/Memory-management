3b07ce9c6bc20ad0bbd318586e06ebd23d81162c
FirescuOvidiu <ovidiufirescu@gmail.com>
2020-05-06 19:41:47 +0300

    Added comments and minor changes to stopTimer method in BenchMarking

--- CustomAllocator/CustomAllocator/BenchMarking.cpp
+++ CustomAllocator/CustomAllocator/BenchMarking.cpp
@@ -1,20 +1,28 @@
 #include "stdafx.h"
 
-
+/*
+	Method used to start the timer
+*/
 void BenchMarking::startTimer()
 {
+	// Keep track of the time point that we get from the high resolution clock
 	startTimePoint = std::chrono::high_resolution_clock::now();
 }
 
 
+/*
+	Method used to stop the timer
+*/
 void BenchMarking::stopTimer()
 {
+	// Get the current time point 
 	auto endTimePoint = std::chrono::high_resolution_clock::now();
 
-	auto start = std::chrono::time_point_cast<std::chrono::milliseconds>(startTimePoint).time_since_epoch();
-	auto end = std::chrono::time_point_cast<std::chrono::milliseconds>(endTimePoint).time_since_epoch();
+	// Transform the time points into microseconds
+	auto start = std::chrono::time_point_cast<std::chrono::microseconds>(startTimePoint).time_since_epoch().count();
+	auto end = std::chrono::time_point_cast<std::chrono::microseconds>(endTimePoint).time_since_epoch().count();
 
-	totalTime += std::chrono::duration_cast<std::chrono::milliseconds>(end-start).count();
+	totalTime += end - start;
 }
 
 
@@ -25,7 +33,7 @@ BenchMarking::~BenchMarking()
 		benchmarkFile.open("benchmark.txt", std::ofstream::out);
 
 		benchmarkFile << std::setprecision(2) << std::fixed << "\n\n\t" << "<----------------------- BENCHMARKING ----------------------->" << "\n\n";
-		benchmarkFile << "\tTotal time: " << totalTime << "\n\n";
+		benchmarkFile << "\tTotal time: " << totalTime << " microseconds ("<< totalTime*00.1<<" milliseconds).\n\n";
 		benchmarkFile << "\t" << "<----------------------- END ----------------------->";
 
 		benchmarkFile.close();
