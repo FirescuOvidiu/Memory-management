8757f51790a51a02099a4c342e5ebc13bc35fc34
FirescuOvidiu <ovidiufirescu@gmail.com>
2020-06-01 16:00:04 +0300

    Added necessary methods in class BestFit

--- CustomAllocator/CustomAllocator/BestFit.cpp
+++ CustomAllocator/CustomAllocator/BestFit.cpp
@@ -1,2 +1,66 @@
 #include "stdafx.h"
-#include "BestFit.h"
+
+
+BestFit::BestFit(size_t poolSize)
+{
+
+}
+
+
+std::ostream& BestFit::write(std::ostream& output) const
+{
+	// TODO: insert return statement here
+}
+
+
+std::istream& BestFit::read(std::istream& input)
+{
+	// TODO: insert return statement here
+}
+
+
+void* __cdecl BestFit::allocMemory(size_t aSize, int, char const*, int)
+{
+	return nullptr;
+}
+
+
+void __cdecl BestFit::freeMemory(void* aBlock, int)
+{
+
+}
+
+std::pair<int, int> BestFit::getCurrentState() const
+{
+	return std::pair<int, int>();
+}
+
+
+void BestFit::showCurrentState() const
+{
+
+}
+
+
+BestFit::~BestFit()
+{
+
+}
+
+
+bool BestFit::checkBadAlloc(size_t aSize)
+{
+	return false;
+}
+
+
+bool BestFit::checkInvalidAddress(void* aBlock, const std::set<PoolElement>::iterator& it)
+{
+	return false;
+}
+
+
+void BestFit::checkMemoryLeaks()
+{
+
+}
--- CustomAllocator/CustomAllocator/BestFit.h
+++ CustomAllocator/CustomAllocator/BestFit.h
@@ -1,7 +1,31 @@
 #pragma once
+#include "stdafx.h"
 
 
-class BestFit
+class BestFit : public Strategy
 {
+public:
+	BestFit(size_t poolSize);
 
+	std::ostream& write(std::ostream& output) const override;
+	std::istream& read(std::istream& input) override;
+
+	void* __cdecl allocMemory(size_t aSize, int /*aBlockUse*/, char const* /*aFileName*/, int /*aLineNumber*/) override;
+	void __cdecl freeMemory(void* aBlock, int /*aBlockUse*/) override;
+
+	std::pair<int, int> getCurrentState() const;
+	void showCurrentState() const;
+
+	~BestFit();
+
+private:
+	bool checkBadAlloc(size_t aSize);
+	bool checkInvalidAddress(void* aBlock, const std::set<PoolElement>::iterator& it);
+	void checkMemoryLeaks();
+
+private:
+	char* startAddress;					// Start address of the memory pool
+	std::list<PoolElement> mAvailable;	// Stores the memory unallocated sorted descending by the size
+	std::set<PoolElement> mAllocated;	// Stores the memory allocated sorted by the address
+	std::size_t poolSize;				// The size of the memory pool
 };
